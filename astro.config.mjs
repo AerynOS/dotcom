eb7d3cf7e42018ea9a37e8a5ab03e14d6993083f
Cameron Braid <cameron@drivenow.com.au>
2025-02-18 02:18:20 +1100

    basic skeleton before porting content from serpentos.com

---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+// import compressor from "astro-compressor"
+
+import mdx from "@astrojs/mdx"
+
+// https://astro.build/config
+export default defineConfig({
+  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
+  site: "https://aerynos.com",
+  image: {
+    domains: ["images.unsplash.com"],
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+    fallback: {},
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  prefetch: true,
+  integrations: [
+    tailwind(),
+
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `fr` after `https://aerynos.com/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          // fr: "fr",
+        },
+      },
+    }),
+
+    // pre-compress them generated files - requires web server to be configured to look for these first
+    // compressor({
+    //   gzip: false,
+    //   brotli: true,
+    // }),
+
+    mdx()
+  ],
+  experimental: {
+    clientPrerender: true,
+    svg: true,
+  },
+})
---
+++ src/content/blog/en/post-1.md
@@ -0,0 +1,15 @@
+---
+title: "Blog post 1"
+description: "todo"
+author: "AerynOS"
+authorImage: null
+authorImageAlt: null
+pubDate: 2024-02-06
+cardImage: null
+cardImageAlt: null
+readTime: 2
+tags: ["linux" ]
+contents: [
+  "blah"
+]
+---
